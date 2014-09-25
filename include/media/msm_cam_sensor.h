f00072d25dd4b18f8dd6635331e37dea5b3d3d4c
Ethan Chen <intervigil@gmail.com>
2014-09-25 12:57:31 -0700

    misc: Import Yulong/Coolpad CP8675-I01-P0 device
    * Original package version: ???
    * Original CAF tag: LNX.LA.3.7.3.c2-00600-8939.0
    * Current CAF tag: LA.BR.1.1.3_rb1.10
    
    Change-Id: I4c3999c82e50ffd8d62cd46e25947bbd9eedfdb4

--- include/media/msm_cam_sensor.h
+++ include/media/msm_cam_sensor.h
@@ -1,3 +1,7 @@
+#ifdef CONFIG_MACH_YULONG
+#include <media/msm_cam_sensor_yulong.h>
+#else
+
 #ifndef __LINUX_MSM_CAM_SENSOR_H
 #define __LINUX_MSM_CAM_SENSOR_H
 
@@ -777,3 +781,4 @@ struct msm_flash_cfg_data_t32 {
 #endif
 
 #endif /* __LINUX_MSM_CAM_SENSOR_H */
+#endif
